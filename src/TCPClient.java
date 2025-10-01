0492422db60742676c9b78a52d22f479feb9c811
karelinejones <lucky52603@gmail.com>
2025-10-01 12:49:02 -0400

    cases

--- src/TCPClient.java
+++ src/TCPClient.java
@@ -4,9 +4,8 @@ import java.io.ObjectInputStream;
 import java.util.Scanner;
 
 public class TCPClient {
-    public static void main(String[] args) {
-        System.out.println("");
-    }
+    public static void main(String[] args) throws Exception {
+
 
     Scanner keyboard = new Scanner(System.in);
     String input = keyboard.nextLine();
@@ -27,13 +26,47 @@ public class TCPClient {
                 for (Map.Entry<String, String> entry : commands.entrySet()) {
                     System.out.println("  " + entry.getKey() + ": " + entry.getValue());
                 }
+                channel.connect(new InetSocketAddress(args[0], serverPort));
+                commandBuffer.putChar(command);
+                commandBuffer.flip();
+                channel.write(commandBuffer);
                 break;
             case 'Delete':
+                BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+                JFileChooser JFileChooser = new JFileChooser();
+                JFileChooser.setTitle("pick a file");
+                File selectedFile = fileChooser.showOpenDialog(primaryStage);
+                String selecedFile = br.readLine();
+                File file = new File(selecedFile);
+
+                if (file.delete()) {
+                    System.out.println("File deleted");
+                }
+                else {
+                    System.out.println("Failed to delete file");
+                }
+                channel.connect(new InetSocketAddress(args[0], serverPort));
+                commandBuffer.putChar(command);
+                commandBuffer.flip();
+                channel.write(commandBuffer);
                 break;
             case 'Rename':
+                JFileChooser JFileChooser = new JFileChooser();
+                JFileChooser.setTitle("pick a file");
+                File selectedFile = fileChooser.showOpenDialog(primaryStage);
 
+                if (selectedFile != null) {
+                    System.out.println("Selected file: " + selectedFile.getAbsolutePath());
+                } else {
+                    System.out.println("File selection cancelled.");
                 }
+                System.out.println("what name do you want to change it to?");
+                selectedFile.renameTo(new file(scanner.nextLine()));
+                channel.connect(new InetSocketAddress(args[0], serverPort));
+                commandBuffer.putChar(command);
+                commandBuffer.flip();
+                channel.write(commandBuffer);
                 break;
             case 'Download':
 
@@ -47,9 +80,18 @@ public class TCPClient {
                     System.out.println("Selected file: " + selectedFile.getAbsolutePath());
                 } else {
                     System.out.println("File selection cancelled.");
+                }
+                channel.connect(new InetSocketAddress(args[0], serverPort));
+                commandBuffer.putChar(command);
+                commandBuffer.flip();
+                channel.write(commandBuffer);
                 break;
                     case 'Quit':
-
+                    System.exit(0);
+                    channel.connect(new InetSocketAddress(args[0], serverPort));
+                    commandBuffer.putChar(command);
+                    commandBuffer.flip();
+                    channel.write(commandBuffer);
                         break;
 
         }
